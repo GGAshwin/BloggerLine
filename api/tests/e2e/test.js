db0fdf3edc83170184c89e8cc55c2d22499775f8
I528709 <sthuthi.b@sap.com>
2024-06-01 08:56:29 +0530

    Selenium testing

---
+++ api/tests/e2e/test.js
@@ -0,0 +1,176 @@
+import { expect } from 'chai';
+import { Builder, By, until } from 'selenium-webdriver';
+import { describe, it, before, after } from 'mocha';
+
+let driver;
+
+describe('UI tests', function () {
+  this.timeout(30000);
+
+  before(async function () {
+    driver = await new Builder().forBrowser('chrome').build();
+  });
+
+  after(async function () {
+    await driver.quit();
+  });
+
+  it('should have the correct page title', async function () {
+    await driver.get('http://localhost:3000');
+    const title = await driver.getTitle();
+    expect(title).to.equal('Bloggerline');
+  });
+
+  it('should click on the LOGIN button', async function () {
+    await driver.get('http://localhost:3000');
+
+    const loginLink = await driver.findElement(By.linkText('LOGIN'));
+    await loginLink.click();
+  });
+
+  it('should log in successfully with valid credentials', async function () {
+  
+    const usernameInput = await driver.findElement(By.css('input[placeholder="Enter your Username..."]'));
+    await usernameInput.sendKeys('ram'); 
+
+    
+    const passwordInput = await driver.findElement(By.css('input[placeholder="Enter your password..."]'));
+    await passwordInput.sendKeys('ram'); 
+
+    
+    const loginButton = await driver.findElement(By.css('.loginButton'));
+    await loginButton.click();
+  });
+
+//   it('should scroll to the bottom of the page and add a comment', async function () {
+//     // Navigate to the SinglePost page
+//     await driver.get('http://localhost:3000/post/6659fa359cc87cacc0b6d566');
+
+//     await driver.sleep(1000);
+
+//     // Scroll to the bottom of the page
+//     await driver.executeScript('window.scrollTo(0, document.body.scrollHeight)');
+
+//     // Wait for a brief moment to ensure scrolling is complete
+//     await driver.sleep(3000);
+
+//     // Wait for the comment input field to be visible
+//     const commentInput = await driver.wait(until.elementLocated(By.css('.commentInput')), 10000);
+
+//     // Add a comment
+//     await commentInput.sendKeys('This is a test comment');
+
+//     // Click the post comment button
+//     const postCommentButton = await driver.findElement(By.css('button[type="submit"]'));
+//     await postCommentButton.click();
+
+//     // Wait for the comment to be added
+//     await driver.wait(until.elementLocated(By.xpath('//div[@class="comment"]/p[contains(text(), "This is a test comment")]')), 5000);
+//   });
+  
+
+it('should select a rating and submit it', async function () {
+    // Navigate to the SinglePost page
+    await driver.get('http://localhost:3000/post/665a1b476a22aaee3ed5869c');
+  
+    // Scroll to the bottom of the page
+    await driver.executeScript('window.scrollTo(0, document.body.scrollHeight)');
+  
+    // Wait for a brief moment to ensure scrolling is complete
+    await driver.sleep(1000);
+  
+    // Find the select element for rating
+    const ratingSelect = await driver.findElement(By.css('.select-rating-container select'));
+  
+    // Select a rating (e.g., 5 stars)
+    await ratingSelect.sendKeys('5');
+  
+    // Find and click the submit rating button
+    const submitRatingButton = await driver.findElement(By.css('.select-rating-container button'));
+    await submitRatingButton.click();
+  
+    // Wait for the rating to be submitted (you may need to adjust the selector based on your UI)
+    // await driver.wait(until.elementLocated(By.xpath('//div[contains(text(), "Thank you for your rating!")]')), 5000);
+  });
+  
+
+  it('should display search input and category dropdown', async function () {
+    await driver.get('http://localhost:3000');
+
+    const searchInput = await driver.findElement(By.id('search'));
+    expect(await searchInput.isDisplayed()).to.be.true;
+
+    const categoryDropdown = await driver.findElement(By.id('categories'));
+    expect(await categoryDropdown.isDisplayed()).to.be.true;
+  });
+
+  it('should allow entering text into the search input', async function () {
+    await driver.get('http://localhost:3000');
+
+    const searchInput = await driver.findElement(By.id('search'));
+    await searchInput.sendKeys('React');
+    const value = await searchInput.getAttribute('value');
+    expect(value).to.equal('React');
+  });
+
+  it('should click on the write button', async function () {
+    await driver.get('http://localhost:3000');
+
+    const loginLink = await driver.findElement(By.linkText('WRITE'));
+    await loginLink.click();
+  });
+
+  // it('should write and post a blog', async function () {
+  //   await driver.get('http://localhost:3000/write');
+
+  //   // Wait for the write form to be visible
+  //   await driver.wait(until.elementLocated(By.css('.writeForm')), 5000);
+
+  //   // Enter the blog title
+  //   const titleInput = await driver.findElement(By.css('input[placeholder="Title"]'));
+  //   await titleInput.sendKeys('Test Blog Title');
+
+  //   // Enter the blog description
+  //   const descInput = await driver.findElement(By.css('textarea[placeholder="Tell your story..."]'));
+  //   await descInput.sendKeys('This is a test blog description.');
+
+  //   // Enter the image URL (optional)
+  //   const imageInput = await driver.findElement(By.css('input[placeholder="Image URL (Optional)"]'));
+  //   await imageInput.sendKeys('https://example.com/test-image.jpg');
+
+  //   // Add a category tag
+  //   const tagInput = await driver.findElement(By.css('.ReactTags__tagInputField'));
+  //   await tagInput.sendKeys('Test Category');
+  //   await tagInput.sendKeys('\uE007'); // Press Enter key
+
+  //   // Submit the blog post
+  //   const submitButton = await driver.findElement(By.css('.writeSubmit'));
+  //   await submitButton.click();
+
+  //   // Wait for redirection to the new post
+  //   await driver.wait(until.urlContains('/post/'), 5000);
+
+  //   // Verify the post title on the new page
+  //   const postTitle = await driver.findElement(By.css('.postTitle'));
+  //   const postTitleText = await postTitle.getText();
+  //   expect(postTitleText).to.equal('Test Blog Title');
+  // });
+  
+
+
+  // it('should log out successfully', async function () {
+  //   // Assuming we are now logged in and on some other page
+  //   // For demonstration purposes, let's navigate to the settings page
+  //   await driver.get('http://localhost:3000');
+
+  //   // Wait until the LOGOUT link is visible
+  //   const logoutLink = await driver.wait(until.elementLocated(By.linkText('LOGOUT')), 5000);
+
+  //   // Click the LOGOUT link
+  //   await logoutLink.click();
+
+  //   // Verify if the user is redirected to the login page
+  //   const currentUrl = await driver.getCurrentUrl();
+  //   expect(currentUrl).to.equal('http://localhost:3000');
+  // });
+});
--- client/src/Components/singlePost/SinglePost.jsx
+++ client/src/Components/singlePost/SinglePost.jsx
@@ -1,294 +1,294 @@
-import axios from "axios";
-import { useContext, useEffect, useState } from "react";
-import { Link, useLocation } from "react-router-dom";
-import "./singlePost.css";
-import { Context } from "../../context/Context";
+  import axios from "axios";
+  import { useContext, useEffect, useState } from "react";
+  import { Link, useLocation } from "react-router-dom";
+  import "./singlePost.css";
+  import { Context } from "../../context/Context";
 
-export default function SinglePost() {
-  const location = useLocation();
-  const path = location.pathname.split("/")[2];
-  const [post, setPost] = useState([]);
-  const [updateMode, setUpdateMode] = useState(false);
-  const [title, setTitle] = useState("");
-  const [desc, setDesc] = useState("");
-  const [photo, setPhoto] = useState("");
-  const [tags, setTags] = useState([]);
-  const [newComment, setNewComment] = useState("");
-  const [avgReviews, setAvgReviews] = useState([]);
-  const [selectedRating, setSelectedRating] = useState(0); // New state for selected rating
-  const [errorMessage, setErrorMessage] = useState(""); // State for error message
-  const { user } = useContext(Context);
+  export default function SinglePost() {
+    const location = useLocation();
+    const path = location.pathname.split("/")[2];
+    const [post, setPost] = useState([]);
+    const [updateMode, setUpdateMode] = useState(false);
+    const [title, setTitle] = useState("");
+    const [desc, setDesc] = useState("");
+    const [photo, setPhoto] = useState("");
+    const [tags, setTags] = useState([]);
+    const [newComment, setNewComment] = useState("");
+    const [avgReviews, setAvgReviews] = useState([]);
+    const [selectedRating, setSelectedRating] = useState(0); // New state for selected rating
+    const [errorMessage, setErrorMessage] = useState(""); // State for error message
+    const { user } = useContext(Context);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await axios.get(
-        process.env.REACT_APP_API + "/post/" + path
-      );
-      setPost(response.data);
-      setTitle(response.data.title);
-      setDesc(response.data.desc);
-      setTags(response.data.categories);
-      setPhoto(response.data.photo);
+    useEffect(() => {
+      const fetchData = async () => {
+        const response = await axios.get(
+          process.env.REACT_APP_API + "/post/" + path
+        );
+        setPost(response.data);
+        setTitle(response.data.title);
+        setDesc(response.data.desc);
+        setTags(response.data.categories);
+        setPhoto(response.data.photo);
 
-      // Fetch reviews for the post
-      const reviewResponse = await axios.get(
-        process.env.REACT_APP_API + "/post/" + path + "/reviews"
-      );
-      setAvgReviews(reviewResponse.data.averageRating);
-    };
+        // Fetch reviews for the post
+        const reviewResponse = await axios.get(
+          process.env.REACT_APP_API + "/post/" + path + "/reviews"
+        );
+        setAvgReviews(reviewResponse.data.averageRating);
+      };
 
-    fetchData();
-  }, [path, newComment]);
+      fetchData();
+    }, [path, newComment]);
 
-  // console.log(user);
+    // console.log(user);
 
-  const handleReviewSubmit = async (e) => {
-    e.preventDefault();
-
-    try {
-      const response = await axios.post(
-        `${process.env.REACT_APP_API}/post/${post._id}/reviews`,
-        { userId: user._id, rating: selectedRating } // Assuming rating is always 5
-      );
-      // console.log(response);
-      // Update the post object with the new review
-      setPost({ ...post, reviews: [...post.reviews, response.data] });
-    } catch (error) {
-      console.error("Error posting review:", error);
-      console.log(error.response.data.message);
-      setErrorMessage(error.response.data.message);
-    }
-  };
-
-  const deletePost = async (e) => {
-    // console.log(user.username);
-    e.preventDefault();
-    const payload = {
-      username: user.user.username,
-    };
-    try {
-      await axios.delete(process.env.REACT_APP_API + "/post/" + path, {
-        data: payload,
-      });
-      window.location.replace("/");
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const handleUpdate = async (e) => {
-    const payload = {
-      username: user.user.user.username,
-      title: title,
-      desc: desc,
-      photo: photo,
-    };
-    try {
-      await axios.put(process.env.REACT_APP_API + "/post/" + path, payload);
-      // window.location.reload()
-      setUpdateMode(false);
+    const handleReviewSubmit = async (e) => {
       e.preventDefault();
-    } catch (error) {
-      console.log(error);
-    }
-  };
 
-  const tagsArr =
-    tags &&
-    tags.map((tag) => {
-      return (
-        <Link className="link" to={`/post?cat=${tag}`}>
-          {tag}
-        </Link>
-      );
-    });
+      try {
+        const response = await axios.post(
+          `${process.env.REACT_APP_API}/post/${post._id}/reviews`,
+          { userId: user._id, rating: selectedRating } // Assuming rating is always 5
+        );
+        // console.log(response);
+        // Update the post object with the new review
+        setPost({ ...post, reviews: [...post.reviews, response.data] });
+      } catch (error) {
+        console.error("Error posting review:", error);
+        console.log(error.response.data.message);
+        setErrorMessage(error.response.data.message);
+      }
+    };
 
-  const handleCommentSubmit = async (e) => {
-    e.preventDefault();
-    if (!newComment) {
-      return; // Prevent empty review submission
-    }
-    try {
-      const response = await axios.post(
-        `${process.env.REACT_APP_API}/post/${post._id}/comments`,
-        { content: newComment, author: user.user.username }
-      );
-      // Update the post object with the new comment (assuming response contains the comment)
-      setPost({ ...post, comments: [...post.comments, response.data] });
+    const deletePost = async (e) => {
+      // console.log(user.username);
+      e.preventDefault();
+      const payload = {
+        username: user.user.username,
+      };
+      try {
+        await axios.delete(process.env.REACT_APP_API + "/post/" + path, {
+          data: payload,
+        });
+        window.location.replace("/");
+      } catch (error) {
+        console.log(error);
+      }
+    };
 
-      setNewComment(""); // Clear comment input
-    } catch (error) {
-      console.error("Error posting comment:", error);
-    }
-  };
+    const handleUpdate = async (e) => {
+      const payload = {
+        username: user.user.user.username,
+        title: title,
+        desc: desc,
+        photo: photo,
+      };
+      try {
+        await axios.put(process.env.REACT_APP_API + "/post/" + path, payload);
+        // window.location.reload()
+        setUpdateMode(false);
+        e.preventDefault();
+      } catch (error) {
+        console.log(error);
+      }
+    };
 
-  const [commentsToShow, setCommentsToShow] = useState(2);
+    const tagsArr =
+      tags &&
+      tags.map((tag) => {
+        return (
+          <Link className="link" to={`/post?cat=${tag}`}>
+            {tag}
+          </Link>
+        );
+      });
 
-  const handleLoadMoreComments = () => {
-    setCommentsToShow((prev) => prev + 2);
-  };
+    const handleCommentSubmit = async (e) => {
+      e.preventDefault();
+      if (!newComment) {
+        return; // Prevent empty review submission
+      }
+      try {
+        const response = await axios.post(
+          `${process.env.REACT_APP_API}/post/${post._id}/comments`,
+          { content: newComment, author: user.user.username }
+        );
+        // Update the post object with the new comment (assuming response contains the comment)
+        setPost({ ...post, comments: [...post.comments, response.data] });
 
-  return (
-    <div className="singlePost">
-      <div className="singlePostWrapper">
-        {updateMode ? (
-          <input
-            type="text"
-            value={photo}
-            className="singlePostTitleInput"
-            placeholder="Image URL"
-            autoFocus
-            onChange={(e) => {
-              setPhoto(e.target.value);
-            }}
-          />
-        ) : (
-          post.photo && (
-            <img className="singlePostImg" src={post.photo} alt="" />
-          )
-        )}
+        setNewComment(""); // Clear comment input
+      } catch (error) {
+        console.error("Error posting comment:", error);
+      }
+    };
 
-        {updateMode ? (
-          <input
-            type="text"
-            value={title}
-            className="singlePostTitleInput"
-            autoFocus
-            onChange={(e) => {
-              setTitle(e.target.value);
-            }}
-          />
-        ) : (
-          <h1 className="singlePostTitle">
-            {title}
-            {post.username === user?.username && (
-              <div className="singlePostEdit">
-                <i
-                  className="singlePostIcon far fa-edit"
-                  onClick={() => {
-                    setUpdateMode(true);
-                  }}
-                ></i>
-                <i
-                  className="singlePostIcon far fa-trash-alt"
-                  onClick={deletePost}
-                ></i>
+    const [commentsToShow, setCommentsToShow] = useState(2);
+
+    const handleLoadMoreComments = () => {
+      setCommentsToShow((prev) => prev + 2);
+    };
+
+    return (
+      <div className="singlePost">
+        <div className="singlePostWrapper">
+          {updateMode ? (
+            <input
+              type="text"
+              value={photo}
+              className="singlePostTitleInput"
+              placeholder="Image URL"
+              autoFocus
+              onChange={(e) => {
+                setPhoto(e.target.value);
+              }}
+            />
+          ) : (
+            post.photo && (
+              <img className="singlePostImg" src={post.photo} alt="" />
+            )
+          )}
+
+          {updateMode ? (
+            <input
+              type="text"
+              value={title}
+              className="singlePostTitleInput"
+              autoFocus
+              onChange={(e) => {
+                setTitle(e.target.value);
+              }}
+            />
+          ) : (
+            <h1 className="singlePostTitle">
+              {title}
+              {post.username === user?.username && (
+                <div className="singlePostEdit">
+                  <i
+                    className="singlePostIcon far fa-edit"
+                    onClick={() => {
+                      setUpdateMode(true);
+                    }}
+                  ></i>
+                  <i
+                    className="singlePostIcon far fa-trash-alt"
+                    onClick={deletePost}
+                  ></i>
+                </div>
+              )}
+            </h1>
+          )}
+
+          {/* Tags section */}
+          <div className="tag-section">
+            <ul style={{ color: "white" }}>{tagsArr}</ul>
+          </div>
+
+          <div className="singlePostInfo">
+            <span>
+              Author:
+              <b className="singlePostAuthor">
+                <Link className="link" to={`/?username=${post.username}`}>
+                  {post.username}
+                </Link>
+              </b>
+            </span>
+            <span>{new Date(post.createdAt).toDateString()}</span>
+          </div>
+          {updateMode ? (
+            <textarea
+              className="singlePostDesc"
+              style={{ color: "black" }}
+              value={desc}
+              onChange={(e) => {
+                setDesc(e.target.value);
+              }}
+            />
+          ) : (
+            <p className="singlePostDesc">{desc}</p>
+          )}
+          {updateMode && (
+            <button className="singlePostButton" onClick={handleUpdate}>
+              Update Post
+            </button>
+          )}
+        </div>
+
+        {/* Reviews section */}
+        {/* {console.log(reviews)} */}
+        <div class="heading">
+          <h2>REVIEWS</h2>
+        </div>
+
+        <div class="feedback">
+          <div class="average-review-section">
+            {avgReviews && (
+              <div className="reviewSection">
+                <h3>Average Rating</h3>
+                <p>{avgReviews}/5</p>
               </div>
             )}
-          </h1>
-        )}
+          </div>
 
-        {/* Tags section */}
-        <div className="tag-section">
-          <ul style={{ color: "white" }}>{tagsArr}</ul>
-        </div>
-
-        <div className="singlePostInfo">
-          <span>
-            Author:
-            <b className="singlePostAuthor">
-              <Link className="link" to={`/?username=${post.username}`}>
-                {post.username}
-              </Link>
-            </b>
-          </span>
-          <span>{new Date(post.createdAt).toDateString()}</span>
-        </div>
-        {updateMode ? (
-          <textarea
-            className="singlePostDesc"
-            style={{ color: "black" }}
-            value={desc}
-            onChange={(e) => {
-              setDesc(e.target.value);
-            }}
-          />
-        ) : (
-          <p className="singlePostDesc">{desc}</p>
-        )}
-        {updateMode && (
-          <button className="singlePostButton" onClick={handleUpdate}>
-            Update Post
-          </button>
-        )}
-      </div>
-
-      {/* Reviews section */}
-      {/* {console.log(reviews)} */}
-      <div class="heading">
-        <h2>REVIEWS</h2>
-      </div>
-
-      <div class="feedback">
-        <div class="average-review-section">
-          {avgReviews && (
-            <div className="reviewSection">
-              <h3>Average Rating</h3>
-              <p>{avgReviews}/5</p>
-            </div>
-          )}
-        </div>
-
-        <div class="give-review-section">
-              <h3>Give Your Rating</h3>
-              <div class="select-rating-container">
-                <select
-                  value={selectedRating}
-                  onChange={(e) => setSelectedRating(e.target.value)}
-                >
-                  <option value={0}>Select Rating</option>
-                  <option value={1}>1</option>
-                  <option value={2}>2</option>
-                  <option value={3}>3</option>
-                  <option value={4}>4</option>
-                  <option value={5}>5</option>
-                </select>
-                <button onClick={handleReviewSubmit}>Submit Rating</button>
+          <div class="give-review-section">
+                <h3>Give Your Rating</h3>
+                <div class="select-rating-container">
+                  <select
+                    value={selectedRating}
+                    onChange={(e) => setSelectedRating(e.target.value)}
+                  >
+                    <option value={0}>Select Rating</option>
+                    <option value={1}>1</option>
+                    <option value={2}>2</option>
+                    <option value={3}>3</option>
+                    <option value={4}>4</option>
+                    <option value={5}>5</option>
+                  </select>
+                  <button onClick={handleReviewSubmit}>Submit Rating</button>
+                </div>
+                <div>{errorMessage && errorMessage}</div>
               </div>
-              <div>{errorMessage && errorMessage}</div>
-            </div>
 
-        <div class="comments-section">
-          <h3>Comments</h3>
-          {post.comments && (
-            <div class="commentSection">
-              {post.comments.slice(0, commentsToShow).map((comment) => (
-                <div key={comment._id} className="comment">
-                  <p className="commentAuthor">{comment.author}</p>
-                  <p>{comment.content}</p>
-                </div>
-              ))}
-              {post.comments.length > commentsToShow && (
-                <button
-                  className="load-more-button"
-                  onClick={handleLoadMoreComments}
-                >
-                  Load More
-                </button>
-              )}
-            </div>
+          <div class="comments-section">
+            <h3>Comments</h3>
+            {post.comments && (
+              <div class="commentSection">
+                {post.comments.slice(0, commentsToShow).map((comment) => (
+                  <div key={comment._id} className="comment">
+                    <p className="commentAuthor">{comment.author}</p>
+                    <p>{comment.content}</p>
+                  </div>
+                ))}
+                {post.comments.length > commentsToShow && (
+                  <button
+                    className="load-more-button"
+                    onClick={handleLoadMoreComments}
+                  >
+                    Load More
+                  </button>
+                )}
+              </div>
+            )}
+          </div>
+
+          {user && (
+            <>
+              <div class="post-comment-section">
+                <h3>Post Your Comment</h3>
+                <form onSubmit={handleCommentSubmit}>
+                  <div class="commentInput-container">
+                    <textarea
+                      className="commentInput"
+                      placeholder="Add a comment..."
+                      value={newComment}
+                      onChange={(e) => setNewComment(e.target.value)}
+                    />
+                  </div>
+                  <button type="submit">Post Comment</button>
+                </form>
+              </div>
+            </>
           )}
         </div>
-
-        {user && (
-          <>
-            <div class="post-comment-section">
-              <h3>Post Your Comment</h3>
-              <form onSubmit={handleCommentSubmit}>
-                <div class="commentInput-container">
-                  <textarea
-                    className="commentInput"
-                    placeholder="Add a comment..."
-                    value={newComment}
-                    onChange={(e) => setNewComment(e.target.value)}
-                  />
-                </div>
-                <button type="submit">Post Comment</button>
-              </form>
-            </div>
-          </>
-        )}
       </div>
-    </div>
-  );
-}
+    );
+  }
