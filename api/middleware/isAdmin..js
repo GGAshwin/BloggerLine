8e32868f9060c4ec1e3d87d7619dd5459db58c45
G G ASHWIN PRABHU <65995972+GGAshwin@users.noreply.github.com>
2024-06-05 22:29:53 +0530

    Made approval and dissapproval (#14)

---
+++ api/middleware/isAdmin..js
@@ -0,0 +1,33 @@
+const jwt = require("jsonwebtoken");
+const jwtSecret = "very_secret_token";
+
+function verifyAdmin(req, res, next) {
+  console.log("called");
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  const token = authHeader.split(" ")[1];
+  try {
+    // Verify the JWT token using your secret key
+    const decoded = jwt.verify(token, jwtSecret);
+    console.log(decoded);
+    // Check if the decoded payload contains 'role' with value "admin"
+    if (decoded.role !== "admin") {
+      return res.status(403).json({ message: "Forbidden (Not Admin)" });
+    }
+
+    // If valid admin token, attach decoded user data to the request object
+    req.user = decoded;
+
+    // Continue to the next middleware or route handler
+    next();
+  } catch (err) {
+    // Handle JWT verification errors
+    console.error(err);
+    return res.status(401).json({ message: "Unauthorized (Invalid token)" });
+  }
+}
+
+module.exports = verifyAdmin;
